9b0417a36e76906e401ce9dd32c11118c8a7e0ca
Rakesh <arorark77@gmail.com>
2020-10-07 00:45:33 -0400

    Terraform example

---
+++ terraform/EC2.tf
@@ -0,0 +1,51 @@
+#--------Provider---------------#
+
+provider "aws" {
+region = "us-east-1"
+}
+
+#---------Resource_aws_instance-------------#
+
+resource "aws_instance" "example" {
+ami = "ami-0323c3dd2da7fb37d"
+instance_type = "t2.nano"
+vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+user_data = <<EOF
+#!/bin/bash
+echo "Hello, World" > index.html 
+nohup busybox httpd -f -p "${var.server_port}" &
+EOF
+
+}
+
+#-------------Resource_aws_security-group-----------------#
+
+resource "aws_security_group" "instance" {
+name = "terraform-example-instance"
+
+ingress {
+	from_port = "${var.server_port}"
+	to_port = "${var.server_port}"
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+
+lifecycle {
+create_before_destroy = true
+}
+}
+
+#---------------variable---------------------------------#
+
+variable "server_port" {
+description = "the port the server will use for http requests"
+default = 8080
+}
+
+#------------------Output-----------------------------#
+
+output "public_ip" {
+value = "${aws_instance.example.public_ip}"
+}
+
